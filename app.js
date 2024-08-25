56552e0beab30c944646b0549291a5a596dbd3ca
Ahmad <ahmedgeger@gmail.com>
2024-08-25 17:50:19 +0300

    click eventlistner;

--- app.js
+++ app.js
@@ -3,83 +3,94 @@ function playSound(keyCode) {
   
     switch (keyCode) {
       case 65: 
-        audio = document.getElementById('keyA');
-        break;
+        audio = document.getElementById('keyA')
+        break
 
       case 87: 
-        audio = document.getElementById('keyW');
-        break;
+        audio = document.getElementById('keyW')
+        break
 
       case 83: 
-        audio = document.getElementById('keyS');
-        break;
+        audio = document.getElementById('keyS')
+        break
 
       case 69: 
-        audio = document.getElementById('keyE');
-        break;
+        audio = document.getElementById('keyE')
+        break
 
       case 68: 
-        audio = document.getElementById('keyD');
-        break;
+        audio = document.getElementById('keyD')
+        break
 
         case 70:
-        audio = document.getElementById('keyF');
-        break;
+        audio = document.getElementById('keyF')
+        break
 
         case 84:
-        audio = document.getElementById('keyT');
-        break;
+        audio = document.getElementById('keyT')
+        break
 
         case 71:
-        audio = document.getElementById('keyG');
-        break;
+        audio = document.getElementById('keyG')
+        break
 
         case 89:
-        audio = document.getElementById('keyY');
-        break;
+        audio = document.getElementById('keyY')
+        break
 
         case 72:
-        audio = document.getElementById('keyH');
-        break;
+        audio = document.getElementById('keyH')
+        break
 
         case 85:
-        audio = document.getElementById('keyU');
-        break;
+        audio = document.getElementById('keyU')
+        break
 
         case 74:
-        audio = document.getElementById('keyJ');
-        break;
+        audio = document.getElementById('keyJ')
+        break
 
         case 75:
-        audio = document.getElementById('keyK');
-        break;
+        audio = document.getElementById('keyK')
+        break
 
         case 79:
-        audio = document.getElementById('keyO');
-        break;
+        audio = document.getElementById('keyO')
+        break
 
         case 76:
-        audio = document.getElementById('keyL');
-        break;
+        audio = document.getElementById('keyL')
+        break
 
         case 80:
-        audio = document.getElementById('keyP');
-        break;
+        audio = document.getElementById('keyP')
+        break
 
         case 186:
-        audio = document.getElementById('keySemicolon');
-        break;
+        audio = document.getElementById('keySemi')
+        break
 
       default:
-        return; 
+        return
     }
   
+    
+
     // Reset the sound and play it
     audio.currentTime = 0; 
-    audio.play();          
+    audio.play()
   }
   
   window.addEventListener('keydown', function(e) {
-    playSound(e.keyCode);
-  });
+    playSound(e.keyCode)
+  })
   
+
+
+  
+document.querySelectorAll('.key').forEach(key => {
+  key.addEventListener('click' , function(){
+    const keyCode = parseInt(this.getAttribute('data-key'),10)
+    playSound(keyCode)
+  })
+})
